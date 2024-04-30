73f3712f3720930523841d927830c75c9a749442
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-04-30 20:03:19 +0200

    Create esp32-DeauthDetector.ino

---
+++ esp32-DeauthDetector.ino
@@ -0,0 +1,37 @@
+#include <WiFi.h>
+
+// ! ESP32 Code for detect DEAUTH ATTACKs on YOUR ROUTER with BUZZER !
+BUZZER     -->   ESP32
+BuzzerPin  -->   13
+
+const int BUZZER_PIN = 13; // Pin, na ktorom je pripojený bzučiak
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  // Pripoj sa k Wi-Fi sieti
+  WiFi.begin("YourSSID-OfROuter", "YourPassss");
+
+  // Počkaj, kým sa pripojíme
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi pripojené");
+}
+
+void loop() {
+  // Ak nie sme pripojený na WiFi sieti
+  if (!WiFi.isConnected()) {
+    Serial.print("ESP32 JE Odpojene asi deauth utokom !");
+    WiFi.begin("YourSSID-OfROuter", "YourPassss");
+    delay(300);
+    // Bzučiak
+    digitalWrite(BUZZER_PIN, HIGH); // Zapni bzučiak
+    delay(500); // Počkaj 0,5 sekundy
+    digitalWrite(BUZZER_PIN, LOW); // Vypni bzučiak
+    delay(500); // Počkaj 0,5 sekundy
+  }
+}
